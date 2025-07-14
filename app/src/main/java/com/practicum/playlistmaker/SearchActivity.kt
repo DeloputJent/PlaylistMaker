08ca0192dd82dd4bbbdfb1014c5a93cbccd93884
DeloputJent <enu.w@yandex.ru>
2025-07-14 19:59:27 +0400

    Домашние задание спринта №9 Выполнил: студент курса Android-разработчик, когорты № 31: Евгений Устюжанин

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,14 +1,76 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SearchActivity : AppCompatActivity() {
+
+    var searchedName:String?=""
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(MEMMORY, searchedName)
+    }
+    companion object {
+        const val MEMMORY = ""
+        const val MEMMORY_DEF = ""
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchedName = savedInstanceState.getString(MEMMORY, MEMMORY_DEF)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val pushbackbutton=findViewById<Button>(R.id.fromSearchBackToMain)
+
+        pushbackbutton.setOnClickListener {
+            finish()
+        }
+
+        val clearButton = findViewById<ImageView>(R.id.clearSearchSign)
+        val inputEditText = findViewById<EditText>(R.id.inputSearch)
+
+
+        inputEditText.setText(searchedName)
+
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(inputEditText.windowToken, 0)
+        }
+
+        val textInputControl = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+            override fun afterTextChanged(s: Editable?) {
+                searchedName=s.toString()
+            }
+        }
+        inputEditText.addTextChangedListener(textInputControl)
+    }
+
+    fun clearButtonVisibility(s: CharSequence?): Int {
+            return if (s.isNullOrEmpty()) {
+                View.GONE
+            } else {
+                View.VISIBLE
+            }
     }
 }
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -14,11 +15,37 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val pushbackbutton=findViewById<Button>(R.id.backtoMain)
+        val pushshare = findViewById<Button>(R.id.sharingButton)
+        val pushsupport = findViewById<Button>(R.id.callSupportButton)
+        val pushagreement = findViewById<Button>(R.id.userAgreementButton)
 
         pushbackbutton.setOnClickListener {
-
             finish()
         }
+
+        pushshare.setOnClickListener{
+            val shareIntent=Intent(Intent.ACTION_SEND)
+            shareIntent.type="text/plain"
+            val shareBody="https://practicum.yandex.ru/android-developer/"
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Курс с помощью которого сделано это приложение")
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareBody)
+            startActivity(Intent.createChooser(shareIntent, "Поделиться с помощью"))
+        }
+        pushsupport.setOnClickListener{
+            val sendIntent=Intent(Intent.ACTION_SENDTO)
+            sendIntent.data=Uri.parse("mailto:")
+            sendIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("enu.w@yandex.ru"))
+            sendIntent.putExtra(Intent.EXTRA_SUBJECT, "Сообщение разработчикам и разработчицам приложения Playlist Maker")
+            sendIntent.putExtra(Intent.EXTRA_TEXT,"Спасибо разработчикам и разработчицам за крутое приложение!")
+            startActivity(sendIntent)
+        }
+
+        pushagreement.setOnClickListener{
+            val url = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+            val intent = Intent(Intent.ACTION_VIEW, url)
+            startActivity(intent)
+        }
+
 
     }
 }
