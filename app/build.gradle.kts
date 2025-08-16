2daa5216a1879ab4c658375dfcf7c7d3a39d5e47
DeloputJent <enu.w@yandex.ru>
2025-08-16 07:47:49 +0400

    Домашние задание спринта №11 Выполнил: студент курса Android-разработчик, когорты № 31: Евгений Устюжанин

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.practicum.playlistmaker"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.practicum.playlistmaker"
@@ -36,9 +36,12 @@ android {
 }
 
 dependencies {
-    implementation("com.google.android.material:material:1.6.1")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.google.code.gson:gson:2.13.1")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
---
+++ app/src/main/java/com/practicum/playlistmaker/iTunesResponse.kt
@@ -0,0 +1,6 @@
+package com.practicum.playlistmaker
+
+class iTunesResponse (
+    val resultCount: String,
+    val results: List<Track>
+)
