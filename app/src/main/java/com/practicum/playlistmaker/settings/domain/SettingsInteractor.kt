35b0d019c89c0ec4fb9a514f3b04f4bb090aa603
DeloputJent <enu.w@yandex.ru>
2025-11-23 10:54:06 +0400

    незавершенная задача спринта №16

--- app/src/main/java/com/practicum/playlistmaker/settings/domain/SettingsInteractor.kt
+++ app/src/main/java/com/practicum/playlistmaker/settings/domain/SettingsInteractor.kt
@@ -3,5 +3,4 @@ package com.practicum.playlistmaker.settings.domain
 interface SettingsInteractor {
     fun getThemeSettings(): ThemeSettings
     fun updateThemeSetting(settings: ThemeSettings)
-
 }
--- app/src/main/java/com/practicum/playlistmaker/settings/ui/SettingsViewModel.kt
+++ app/src/main/java/com/practicum/playlistmaker/settings/ui/SettingsViewModel.kt
@@ -1,7 +1,14 @@
 package com.practicum.playlistmaker.settings.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.practicum.playlistmaker.player.ui.PlayerViewModel
 import com.practicum.playlistmaker.settings.domain.SettingsInteractor
+import com.practicum.playlistmaker.settings.domain.ThemeSettings
 import com.practicum.playlistmaker.sharing.domain.api.SharingInteractor
 
 
@@ -9,5 +16,22 @@ class SettingsViewModel(
     private val sharingInteractor: SharingInteractor,
     private val settingsInteractor: SettingsInteractor,
 ) : ViewModel() {
-    // Основной код
+    val settingsState: MutableLiveData<ThemeSettings> = MutableLiveData()
+
+    fun observeThemeState(): MutableLiveData<ThemeSettings> = settingsState
+
+    fun updateSettings(newSettings: ThemeSettings) {
+
+        settingsInteractor.updateThemeSetting(newSettings)
+
+        settingsState.value = newSettings
+    }
+
+    fun getFactory(sharingInteractor: SharingInteractor,
+                  settingsInteractor: SettingsInteractor,): ViewModelProvider.Factory = viewModelFactory {
+        initializer {
+            SettingsViewModel(sharingInteractor, settingsInteractor)
+        }
+    }
+
 }
